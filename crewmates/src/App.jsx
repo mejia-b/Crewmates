979d445de2a24674a14a33ad434d1d16ad2a6bf9
Brenda <mejiab19@gmail.com>
2024-05-07 20:23:05 -0400

    Create: Homepage view

--- crewmates/src/App.jsx
+++ crewmates/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import crewmates from './assets/crewmates.png'
+import spaceship from './assets/spaceship.png'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="whole-page">
+        <div>
+          <div className="home-page">
+            <h1>Welcome to the Crewmate Creators!</h1>
+            <h3>
+              Here is where you can create your very own set of crewmates before sending them off into space!
+            </h3>
+            <img src={crewmates} alt="Group of colorful cartoon astronauts"  className='welcome-images'/>
+            <br />
+            <img src={spaceship} alt="Small spaceship"  className='welcome-images'/>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
